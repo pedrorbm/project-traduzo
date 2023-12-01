2c832e0a0d3b31cd6cb722e4e1c0746ffa561171
Pedro Medeiros <pedropwks@gmail.com>
2023-12-01 01:26:38 -0300

    Feat: Adicionando o método list_dicts da classe LanguageModel

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -3,12 +3,22 @@ from database.db import db
 
 
 class LanguageModel(AbstractModel):
-    _collection = db["langauges"]
+    _collection = db["languages"]
 
     def __init__(self, dict: dict):
         super().__init__(dict)
 
     def to_dict(self):
         result = {"name": self.data["name"], "acronym": self.data["acronym"]}
+
+        return result
+
+    @classmethod
+    def list_dicts(cls):
+        languages = cls._collection.find()
+        result = [
+            cls(data).to_dict()
+            for data in languages
+        ]
 
         return result

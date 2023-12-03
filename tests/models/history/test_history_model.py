a68d1a7ed6d07b0a0c97ab2d67d9cb75244ae0a9
Pedro Medeiros <pedropwks@gmail.com>
2023-12-03 17:51:29 -0300

    Adicionando teste da model HistoryModel

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,43 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+# from src.models.language_model import LanguageModel
+from src.models.history_model import HistoryModel
+
+result = [
+    {
+        "text_to_translate": "Hello, I like videogame",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+    {
+        "text_to_translate": "Do you love music?",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+]
 
 
-# Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history_model = HistoryModel.list_as_json()
+    load = json.loads(history_model)
+    string = type(history_model)
+    test_one = load[1]["text_to_translate"]
+    test_two = load[0]["text_to_translate"]
+
+    try:
+        history_model = HistoryModel.list_as_json()
+        load = json.loads(history_model)
+        string = type(history_model)
+        test_one = load[1]["text_to_translate"]
+        test_two = load[0]["text_to_translate"]
+
+        assert string is str
+
+        assert test_one == result[1]["text_to_translate"]
+
+        assert test_two == result[0]["text_to_translate"]
+    except ValueError:
+        assert string is str
+
+        assert test_one == result[1]["text_to_translate"]
+
+        assert test_two == result[0]["text_to_translate"]
